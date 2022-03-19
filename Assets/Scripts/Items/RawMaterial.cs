fcdc1793b3cd02b8f24fff7a2b09b3feeedb1aa0
Daniel Henrique <danielhenriquecabrallima@gmail.com>
2022-03-19 17:36:54 -0300

    Add generic RawMaterial creation

---
+++ Assets/Scripts/Items/RawMaterial.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Raw Material", menuName = "Inventory System/Items/RawMaterial")]
+
+public class RawMaterial : ItemObject
+{
+    public void Awake() {
+        type = ItemType.RawMaterial; 
+    }
+}
