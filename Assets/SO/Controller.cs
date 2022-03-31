7ae51567ce8ba234851178154fde0fd89c7f20e4
unknown <ewerton.j.c.m@gmail.com>
2022-03-31 20:54:45 -0300

    Scriptable Object

---
+++ Assets/SO/Controller.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MasterController" , menuName = "Controller")]
+public class Controller : ScriptableObject
+{
+    public bool IsCutting = false;
+}
