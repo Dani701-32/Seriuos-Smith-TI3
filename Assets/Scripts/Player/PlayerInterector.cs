1a6d9c007a5aec026d053544f44698bc77c923e6
unknown <ewerton.j.c.m@gmail.com>
2022-03-31 20:57:17 -0300

    modificacoes unity

--- Assets/Scripts/Player/PlayerInterector.cs
+++ Assets/Scripts/Player/PlayerInterector.cs
@@ -1,54 +1,13 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 
 
 public class PlayerInterector : MonoBehaviour
 {
     public GameObject interactor;
-    /*
-    public IInterectable currentInterectable = null;
-    public InputAction playerInteracion;
-    private void OnEnable()
-    {
-        playerInteracion.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerInteracion.Disable();
-    }
-    private void Update()
-    {
-        CheckInterectable(); 
-    }
-    private void CheckInterectable()
-    {
-        if (currentInterectable == null) return;
-        OnEnable(); 
-        playerInteracion.performed  += context => currentInterectable.Interect();
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        //Recebe o objeto que deve ser interagido
-        Debug.Log("ENCOTNROU"); 
-        IInterectable interectable = other.GetComponent<IInterectable>();
-        if (interectable == null) return;
-
-        currentInterectable = interectable;
-
-    }
-    
-
-    private void OnTriggerExit(Collider other)
-    {
-        IInterectable interectable = other.GetComponent<IInterectable>();
-        if (interectable == null) return;
-        if (interectable != currentInterectable) return;
-        OnDisable(); 
-        currentInterectable = null;
-    }
-    */
+    public Controller control;
+    public Spawn spawner;
     void OnTriggerStay(Collider other)
     {
         if(other.gameObject.CompareTag("Interactable")) // poderia usar um bool pra checar apenas uma vez mas sla
@@ -59,6 +18,16 @@ public class PlayerInterector : MonoBehaviour
                 Debug.Log("Test");
             }
         }
-    }
-    
+        else if(other.gameObject.CompareTag("Tree")) // interaçao com a arvore
+        {
+            if(Input.GetKey(KeyCode.F))
+            {
+                control.IsCutting = true;
+                Debug.Log("anothertest");
+                spawner.TimeSpawner();
+            }
+                
+        }
+        
+    } 
 }
