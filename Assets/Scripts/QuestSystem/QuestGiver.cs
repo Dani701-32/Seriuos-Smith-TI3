298cc36b5e6cbc5e76385de13d45eed40e951d5d
Daniel Henrique <85520325+Dani701-32@users.noreply.github.com>
2022-05-14 18:22:55 -0300

    Quest UI controllers

--- Assets/Scripts/QuestSystem/QuestGiver.cs
+++ Assets/Scripts/QuestSystem/QuestGiver.cs
@@ -4,40 +4,73 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestGiver : MonoBehaviour // Aqui vai ser onde vai ter as quest/recepies que vao vir os pedidos e vao ser
-    //passados para o player
+                                        //passados para o player
 {
-    
-    public List<QuestController> controllerQuest = new List<QuestController>();
-    //public List<Quest> quest;
-    public PlayerHolderQuest player;
-
-    public GameObject windowQuest;
-
+    public List<QuestController> tempController = new List<QuestController>();
+    public Queue<QuestController> controllerQuest = new Queue<QuestController>();
+    private QuestController quest;
+    public GameObject questUI;
     public Text titleQuest;
     public Text descriptionQuest;
     public Text goldReward;
-    
 
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private PlayerHolderQuest playerHolder;
+    private void Awake()
+    {
+        questUI.SetActive(false);
+        foreach (QuestController item in tempController)
+        {
+            controllerQuest.Enqueue(item);
+        }
+        GameObject p = GameObject.FindGameObjectWithTag("Player");
+        playerHolder = p.GetComponent<PlayerHolderQuest>();
+        playerMovement = p.GetComponent<PlayerMovement>();
+    }
     public void OpenQuestWindow()
     {
-        windowQuest.SetActive(true);
-
-        titleQuest.text = controllerQuest[0].titleQuest;
-        descriptionQuest.text = controllerQuest[0].descriptionQuest;
-        goldReward.text = controllerQuest[0].goldReward.ToString();
+        playerMovement.enabled = false;
+        questUI.SetActive(true);
+        UpdateQuest();
+    }
+    private void UpdateQuest()
+    {
+        if (controllerQuest.Count > 0)
+        {
+            quest = (quest) ? quest : controllerQuest.Dequeue();
+            titleQuest.text = quest.titleQuest;
+            descriptionQuest.text = $"Quest description: {quest.descriptionQuest}";
+            goldReward.text = $"Reward: {quest.goldReward}";
+        }
+        else
+        {
+            titleQuest.text = "Nothing to see here.";
+            descriptionQuest.text = "No available quest for now";
+            goldReward.text = "";
+        }
     }
 
     public void AcceptQuest()
     {
-        windowQuest?.SetActive(false);
-        controllerQuest[0].questIsActive = true;
-        player.quest = controllerQuest[0];
+        if (!quest || playerHolder.quest)
+        {
+            return;
+        }
+        questUI?.SetActive(false);
+        quest.questIsActive = true;
+        playerMovement.enabled = true;
+        playerHolder.quest = quest;
+        quest = null;
     }
 
-    public void CheckIfOver()
+    public void CloseQuestWindow()
     {
-        player.CheckQuest();
-        controllerQuest.Remove(controllerQuest[0]);
+        playerMovement.enabled = true;
+        questUI.SetActive(false);
     }
-    
+    public void DeclineQuest()
+    {
+        quest = null;
+        UpdateQuest();
+    }
 }
