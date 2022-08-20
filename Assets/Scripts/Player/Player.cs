b771f98fcf41e91139b6db4b3e9397d7a176ab99
Ewerton Jose <ewerton.j.c.m@gmail.com>
2022-08-19 21:02:54 -0300

    Change the name of scripts
    Change the name of the Player holder quest to Playter

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour // Script pra guardar as quests
+{
+    public QuestController quest; 
+    public int energy = 100; // default energy
+    public int gold = 0;
+
+    public bool HasQuest()
+    {
+        return (quest == null) ? false : true;
+    }
+
+    public void AdvanceQuest(Item item)
+    {
+        if (quest.goal.goalType == item.goalType && quest.goal.materialType == item.materialType)
+        {   
+            quest.goal.currentAmount++; 
+            quest.questCompletd = quest.goal.IsReached(); 
+        }
+    }
+
+
+}
